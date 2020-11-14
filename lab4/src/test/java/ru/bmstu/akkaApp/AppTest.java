44ca991541ddb8521de061b373ee5146c8d1064c
Dat Nguyen <datnguyen79198@gmail.com>
2020-11-14 13:34:05 +0300

    lab4 initial packages

---
+++ lab4/src/test/java/ru/bmstu/akkaApp/AppTest.java
@@ -0,0 +1,20 @@
+package ru.bmstu.akkaApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
