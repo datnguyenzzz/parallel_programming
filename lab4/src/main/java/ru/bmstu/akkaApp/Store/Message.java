64dafb12d4bc962eacfde4c000a3a23ff2efb0be
Dat Nguyen <datnguyen79198@gmail.com>
2020-11-14 14:57:22 +0300

    store meesage request from post in json

---
+++ lab4/src/main/java/ru/bmstu/akkaApp/Store/Message.java
@@ -0,0 +1,35 @@
+package ru.bmstu.akkaApp.Store;
+
+import ru.bmstu.akkaApp.Test.Test;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+public class StoreMessage {
+    private int packageID;
+    private ArrayList<Test> tests;
+
+    private final String PACKAGE_ID = "packageID";
+    private final String TESTS = "tests";
+
+    @JsonCreator
+    public StoreMessage(@JsonProperty(PACKAGE_ID) int packageID, @JsonProperty(TESTS) ArrayList<Test> tests) {
+        this.packageID = packageID;
+        this.tests = tests;
+    }
+
+    @Override
+    public String toString() {
+        return "packageID = " + packageID + " : " + tests;
+    }
+
+    public int getPackageID() {
+        return packageID;
+    }
+
+    public ArrayList<Test> getTests() {
+        return tests;
+    }
+}
