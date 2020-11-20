52f78874de4b8320ba0f3865d238ef296787f557
Dat Nguyen <datnguyen79198@gmail.com>
2020-11-20 18:03:20 +0300

    create web server at localhost

--- lab5/src/main/java/ru/bmstu/AkkaPingApp/WebServer.java
+++ lab5/src/main/java/ru/bmstu/AkkaPingApp/WebServer.java
@@ -14,12 +14,6 @@ import akka.http.javadsl.server.Route;
 
 import static akka.http.javadsl.server.Directives.*;
 
-import ru.bmstu.akkaApp.Store.GetMessage;
-import ru.bmstu.akkaApp.Store.StoreActor;
-import ru.bmstu.akkaApp.Test.TestActor;
-import ru.bmstu.akkaApp.Test.TestPackageActor;
-import ru.bmstu.akkaApp.Test.TestPackageMessage;
-
 import akka.pattern.PatternsCS;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
@@ -28,17 +22,51 @@ import akka.stream.javadsl.Flow;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-import static org.asynchttpclient.Dsl.*;
-import org.asynchttpclient.*; //REST api
+import org.asynchttpclient.AsyncHttpClient; //REST api
+import org.asynchttpclient.Dsl; //init client
 
 /**
- * Hello world!
+ * http://localhost:8080/?testUrl=http://rambler.ru&count=20
  *
  */
-public class WebServer
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+class PingServer {
+
+    private PingServer(final ActorSystem system) {
+        storeActor = system.actorOf(Props.create(StoreActor.class));
+    }
+
+}
+
+public class WebServer {
+
+    private static final String domain = "localhost";
+    private static final int port = 8080;
+
+    public static void main( String[] args ) {
+      ActorSystem system = ActorSystem.create("routes");
+
+      final Http http = Http.get(system);
+      final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+      PingServer server = new PingServer(system);
+      final Flow<HttpRequest, HttpResponse, NotUsed> httpFlow = pingServer.getHttpFlow(materializer);
+
+      final CompletionStage<ServerBinding> binding = http.bindAndHandle (
+          httpFlow,
+          ConnectHttp.toHost(domain, port),
+          materializer
+      );
+
+      System.out.println("Server is started at http://localhost:8080");
+
+      System.in.read();
+
+      binding
+            .thenCompose(ServerBinding::unbind)
+            .thenAccept(unbound -> system.terminate());
+
+      System.out.println("Server off");
+
     }
 }
