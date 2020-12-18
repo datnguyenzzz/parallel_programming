ccd5df4ec9b417b9beb79c2155aa469ec152bffe
datnguyen79198 <datnguyen79198@gmail.com>
2020-12-18 23:47:12 +0300

    create socket and get messages from client

--- lab7/src/main/java/ru/bmstu/ProxyApp/Client.java
+++ lab7/src/main/java/ru/bmstu/ProxyApp/Client.java
@@ -2,6 +2,8 @@ package ru.bmstu.ProxyApp.Client;
 
 import org.zeromq.*;
 
+import java.util.Scanner;
+
 public class Client {
     public String ADDRESS = "tcp://localhost:2000";
 
@@ -10,6 +12,22 @@ public class Client {
 
     public static void main(String args[]) {
         System.out.println("Client is trying connect to server");
-        
+        try {
+
+            context = new ZContext();
+            socket = context.createSocket(SocketType.REQ);
+            socket.connect(ADDRESS);
+            Scanner in = new Scanner(System.in);
+            while (true) {
+                String clientMsg = in.nextLine();
+
+                
+
+            }
+
+
+        } catch (ZMQException ex) {
+            ex.printStackTrace();
+        }
     }
 }
