9febaf4cd3fab2a0f11c29c53f7d9ff04db1082e
datnguyen79198 <datnguyen79198@gmail.com>
2020-12-19 10:50:01 +0300

    create socket tcp

--- lab7/src/main/java/ru/bmstu/ProxyApp/Cache.java
+++ lab7/src/main/java/ru/bmstu/ProxyApp/Cache.java
@@ -1,16 +1,52 @@
 package ru.bmstu.ProxyApp.Cache;
 
 import org.zeromq.*;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Cache {
     private final ZContext conn;
 
+    private ZMQ.Socket socket;
+    private ZMQ.Poller data;
+
     private HashMap<Integer,String> cache;
+    private int startPos,endPos;
+
+    public String ADDRESS = "tcp://localhost:2001";
 
     public Cache(ZContext conn) {
         this.conn = conn;
 
         cache = new HashMap<>();
+        Scanner in  = new Scanner(System.in);
+        startPos = in.nextInt();
+        endPos = in.NextInt();
+
+        for (int i=startPos; i<=endPos; i++) {
+            cache.put(i, Integer.toString(i));
+        }
+
+        socket = conn.createSocket(SocketType.DEALER);
+        socket.setHWM(0);
+    }
+
+    public static void main(String args[]) {
+        try {
+            ZContext conn = new ZContext();
+            Cache cache = new Cache(conn);
+            cache.connect();
+            cache.handler();
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    private void connect() {
+        socket.connect(ADDRESS);
+        data = conn.createPoller(1);
+        data.register(socket, ZMQ.Poller.POLLIN);
     }
 }
--- lab7/src/main/java/ru/bmstu/ProxyApp/Client.java
+++ lab7/src/main/java/ru/bmstu/ProxyApp/Client.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class Client {
     public String ADDRESS = "tcp://localhost:2000";
 
-    private static ZContext context; 
+    private static ZContext conn; 
     private static ZMQ.Socket socket;
 
     public static void main(String args[]) {
         System.out.println("Client is trying connect to server");
         try {
 
-            context = new ZContext();
-            socket = context.createSocket(SocketType.REQ);
+            conn = new ZContext();
+            socket = conn.createSocket(SocketType.REQ);
             socket.connect(ADDRESS);
             Scanner in = new Scanner(System.in);
             while (true) {
